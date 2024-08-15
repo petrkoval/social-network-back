6d12bf51c19c1c489c09e09e5b719d5c319ffdcd
Пётр <p.koval@alta-group.ru>
2024-08-15 15:49:13 +1000

    channels & posts tables;

--- migrations/1_init.up.sql
+++ migrations/1_init.up.sql
@@ -10,3 +10,22 @@ CREATE TABLE IF NOT EXISTS tokens
     user_id       uuid PRIMARY KEY NOT NULL REFERENCES users (user_id),
     refresh_token text             NOT NULL
 );
+
+CREATE TABLE IF NOT EXISTS channels
+(
+    channel_id  uuid PRIMARY KEY NOT NULL DEFAULT uuid_generate_v4(),
+    user_id     uuid             NOT NULL REFERENCES users (user_id),
+    created_at  timestamp        NOT NULL DEFAULT now(),
+    title       varchar(32)      NOT NULL,
+    description varchar(256)              DEFAULT NULL
+);
+
+CREATE TABLE IF NOT EXISTS posts
+(
+    post_id    uuid PRIMARY KEY NOT NULL             DEFAULT uuid_generate_v4(),
+    user_id    uuid REFERENCES users (user_id)       DEFAULT NULL,
+    channel_id uuid REFERENCES channels (channel_id) DEFAULT NULL,
+    created_at timestamp        NOT NULL             DEFAULT now(),
+    content    text                                  DEFAULT NULL,
+    images     text[]                                DEFAULT NULL
+);
